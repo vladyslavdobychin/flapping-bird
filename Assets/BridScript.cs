2156df8ef19dff67e311e9bc2cae277c49dfc687
VladDobychin <vladyslav.dobychin@binary-studio.com>
2026-09-24 21:49:36 +0200

    Add pipe prefab

--- Assets/BridScript.cs
+++ Assets/BridScript.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class BridScript : MonoBehaviour
 {
-    public Rigidbody2D myRigidBody2D;
+    public Rigidbody2D rigidbody2D;
+    public float flapStreangth = 12;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            rigidbody2D.linearVelocity = Vector2.up * flapStreangth;
+        }
     }
 }

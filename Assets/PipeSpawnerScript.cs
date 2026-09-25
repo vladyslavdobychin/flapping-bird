2f19d33b4ad3e9743ba51b002332586c596d8df8
VladDobychin <vladyslav.dobychin@binary-studio.com>
2026-09-25 16:49:13 +0200

    Add pipe spawner

---
+++ Assets/PipeSpawnerScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PipeSpawnerScript : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Instantiate(pipe, transform.position, transform.rotation);
+            timer = 0;
+        }
+
+    }
+}
